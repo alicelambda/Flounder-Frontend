107dafcee2abcdb2109f8f0a88c27050bce4c882
alice <alice@alicereuter.com>
2020-07-12 00:35:38 -0500

    created todo slice

---
+++ src/todo/todoSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const todoSlice = createSlice({
+    name: 'todos',
+    initialState: [],
+    reducers: {
+        addTodo: state => {
+            return [
+                ...state,
+                {
+                    id:"heello"
+                }
+            ]
+        }
+    }
+});
+
+export const {addTodo} = todoSlice.actions;
+
+export const selectTodos = state = state.todos;
+
+export const todoReducer = todoSlice.reducer;
